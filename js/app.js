8f207df6c4c5ee4592b401391208399943292872
Hoodoo42 <create@hoodooverse.ca>
2022-07-28 19:01:59 -0600

    a second knowledge check with an axios request where we needed to display a recipe title and image. I was not finished the first knowledge check and it was throwing it off a bit so I commented the first one out, will come back to finish, also to add my code comments

--- js/app.js
+++ js/app.js
@@ -1,17 +1,38 @@
-function recipeFail(error) {
-  document.body.insertAdjacentHTML(`afterbegin`, `<h3>Error!</h3>`);
-}
+// function recipeFail(error) {
+//   document.body.insertAdjacentHTML(`afterbegin`, `<h3>Error!</h3>`);
+// }
 
-function recipeSucess(response) {
-   
+// function recipeSucess(response) {
+//   document.body.insertAdjacentHTML(
+//     `afterbegin`,
+//     `
+//     <h3>${response[`data`]}</h3>`
+//   );
+// }
+
+function chickenSuccess(response){
     document.body.insertAdjacentHTML(`afterbegin`, `
-    <h3>${response[`data`][]}</h3>`);
-
+    <h1>${response[`data`][`meals`][4][`strMeal`]}</h1>
+    <img src="${response[`data`][`meals`][4][`strMealThumb`]}">
+    `)
+}
+function chickenFailure(error){
+    document.body.insertAdjacentHTML(`afterbegin`, `<h3>Error</h3>`)
 }
 
-axios
-  .request({
-    url: `https://www.themealdb.com/api/json/v1/1/random.php`
-  })
-  .then(recipeSucess)
-  .catch(recipeFail);
+
+
+
+// axios
+//   .request({
+//     url: `https://www.themealdb.com/api/json/v1/1/random.php`,
+//   })
+//   .then(recipeSucess)
+//   .catch(recipeFail);
+
+axios.request({
+  url: `https://www.themealdb.com/api/json/v1/1/filter.php`,
+  params: {
+    c: `Chicken`,
+  }
+}).then(chickenSuccess).catch(chickenFailure);
